d3b59347b5d1470b7a8bed4e1eeee0042e78ea66
koti12066 <125641913+koti12066@users.noreply.github.com>
2023-02-27 09:44:12 +0530

    Create test6.java

---
+++ 20211cse0113/test6.java
@@ -0,0 +1,14 @@
+package classobjectDEMO;
+
+public class test6 {
+
+	public static void main(String[] args) {
+		System.out.println("problem begins");
+		int x=5;
+		int y=0;
+		int z=x/y;    //run time mistake
+		System.out.println("end of the program");
+		
+	}
+
+}
